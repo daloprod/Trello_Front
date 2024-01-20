5210296045aa200ee7995accfdd6c8fe4d542b6e
daloprod <ogoula@live.fr>
2024-01-20 23:01:09 +0100

    Modification #2 des fichiers dans src/ pour l'interface Trello

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { AppComponent } from './app/app.component';
 import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+
 
 enableProdMode();
 
 
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
